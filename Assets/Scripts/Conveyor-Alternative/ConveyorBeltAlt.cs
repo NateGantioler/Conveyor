97b484f0e9b9a999ee3e4eb56b0438075678cfab
Mikad02 <caligiacomo0@gmail.com>
2023-04-29 19:45:33 +0200

    fixing a conveyor belt bug
    fixing a bug where packages don't line up with the conveyor belt when changing direction

--- Assets/Scripts/Conveyor-Alternative/ConveyorBeltAlt.cs
+++ Assets/Scripts/Conveyor-Alternative/ConveyorBeltAlt.cs
@@ -9,24 +9,46 @@ public class ConveyorBeltAlt : MonoBehaviour
     public float speed;
 
     private bool packageOnTop;
-    public Rigidbody2D packageRB;
+    private Rigidbody2D packageRB;
+
+    private float distance;
 
     // checks if it's colliding with a package's trigger type collider
     private void OnTriggerEnter2D(Collider2D collision)
     {
         packageRB = collision.GetComponent<Rigidbody2D>();
-        packageOnTop = true;
+        
+        // calculates distance between conveyor belt and package
+        distance = Vector3.Distance(this.transform.position, collision.GetComponent<Transform>().position);
+        Debug.Log($"Distance is {distance}");
+
+        //checks if package is close to the center before making object move
+        if (distance <= 0.1f)
+        {
+            packageOnTop = true;
+        }
+       
     }
+    
     private void OnTriggerExit2D(Collider2D collision)
     {
+        if(distance > 0.1f)
+        {
+            packageOnTop = false;
+            return;
+        }
         packageOnTop = false;
     }
-
+    
     private void FixedUpdate()
     {
         if (packageOnTop)
         {
             MovePackage();
+        }
+        if (distance > 0.1f)
+        {
+            packageOnTop = false;
         }
     }
 
---
+++ Assets/Scripts/Conveyor-Alternative/PackageSpawner.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PackageSpawner : MonoBehaviour
+{
+    // how many seconds need to pass before spawning another package
+    public float spawnInterval;
+
+    // timer that gets decreased as time passes. It gets reset to the interval everytime it reaches 0
+    private float spawnTimer;
+
+    // package gameobject to spawn
+    public GameObject packagePrefab;
+
+    public Transform spawnPosition;
+
+    public void Start()
+    {
+        spawnTimer = spawnInterval;
+    }
+
+    public void Update()
+    {
+        if(spawnTimer <= 0)
+        {
+            // timer gets reset
+            spawnTimer = spawnInterval;
+            // package gets spawned
+            SpawnPackage();
+            return;
+        }
+        if(spawnTimer > 0)
+        {
+            // timer gets decreased in time
+            spawnTimer -= Time.deltaTime;
+        }
+        Debug.Log($"TIMER:{Mathf.FloorToInt(spawnTimer)}");
+    }
+
+    private void SpawnPackage()
+    {
+        Instantiate(packagePrefab, spawnPosition.position, Quaternion.identity);
+    }
+
+}
