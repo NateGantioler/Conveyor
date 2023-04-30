12f464b907207e1df68d0c3409d81f640d12c77d
Mikad02 <caligiacomo0@gmail.com>
2023-04-30 18:44:27 +0200

    c

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -8,19 +8,34 @@ public class UIManager : MonoBehaviour
     static int score;
     static int staticCorrectPackagePoints;
     static int staticWrongPackagePoints;
+    
+    // +1 every time a package gets delivered
+    static int packagesDelivered;
+    static int packagesDropped;
 
+    [Header("Score")]
     public TextMeshProUGUI scoreText;
     public int correctPackagePoints;
     public int wrongPackagePoints;
-
+    
+    [Space]
+    [Header("Timer")]
     public TextMeshProUGUI timerText;
     public float startTimer;
     float timer;
 
     public GameObject endGameScreen;
 
+    [Space]
+    [Header("End game UI")]
+    public TextMeshProUGUI finalScoreText;
+    public TextMeshProUGUI packagesDeliveredText;
+    public TextMeshProUGUI packagesDroppedText;
+
     private void Start()
     {
+        packagesDelivered = 0;
+
         staticCorrectPackagePoints = correctPackagePoints;
         staticWrongPackagePoints = wrongPackagePoints;
 
@@ -44,24 +59,30 @@ public class UIManager : MonoBehaviour
 
     void EndOfGame()
     {
+        finalScoreText.text = score.ToString();
+        packagesDeliveredText.text = packagesDelivered.ToString();
         endGameScreen.SetActive(true);
         Time.timeScale = 0f;
     }
 
+    #region score functions
     public static void AddScore()
     {
+        packagesDelivered += 1;
         score += staticCorrectPackagePoints;
         print("score changed");
     }
     public static void SubtractScore()
     {
+        packagesDropped += 1;
         if (score <= 0) return;
         if (score > 0)
-        { 
+        {
             score -= staticWrongPackagePoints; 
             if(score < 0) { score = 0; }
         }
         print("score changed");
     }
+    #endregion
 
 }
